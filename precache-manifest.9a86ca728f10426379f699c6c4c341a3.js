9a4ea0c7db9d99a991c71f8fdfabcf7233187a62
piyu0809 <phirema1@binghamton.edu>
2019-02-15 18:42:58 -0500

    Updates

---
+++ precache-manifest.9a86ca728f10426379f699c6c4c341a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "deffd6bdfa99e73d1748",
+    "url": "/ChatAll/static/css/main.f67df6bc.chunk.css"
+  },
+  {
+    "revision": "deffd6bdfa99e73d1748",
+    "url": "/ChatAll/static/js/main.deffd6bd.chunk.js"
+  },
+  {
+    "revision": "54e06ce47dd0d04dafe6",
+    "url": "/ChatAll/static/js/1.54e06ce4.chunk.js"
+  },
+  {
+    "revision": "463307368d2fbba44680",
+    "url": "/ChatAll/static/js/runtime~main.46330736.js"
+  },
+  {
+    "revision": "88da968431b0cd7f48f051570c11ce99",
+    "url": "/ChatAll/static/media/icon.88da9684.png"
+  },
+  {
+    "revision": "9a55fe0d7cb8af71697dbd388b5c5e9e",
+    "url": "/ChatAll/static/media/joinroom.9a55fe0d.png"
+  },
+  {
+    "revision": "07b37d9e8af59caf15b0f8e1b49da368",
+    "url": "/ChatAll/static/media/back2.07b37d9e.jpg"
+  },
+  {
+    "revision": "b3e43b3771c6fb86355bc8a8fe1b2b4c",
+    "url": "/ChatAll/index.html"
+  }
+];
